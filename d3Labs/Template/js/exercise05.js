af3235b0ace72b774cd4fed852e090dd7500ad72
mariana <0196539@up.edu.mx>
2020-05-24 15:55:30 -0500

    ejercicio 5 list

--- d3Labs/Template/js/exercise05.js
+++ d3Labs/Template/js/exercise05.js
@@ -1,43 +1,69 @@
-var margin = {top: 10, right: 10, bottom: 100, left:100};
+var margin = {top: 10, right: 10, bottom: 170, left:100};
 var width = 900;
 var height = 600;
-var bottomAxis = d3.axisBottom(x);
+
 var svg = d3.select("#chart-area5").append("svg")
 		.attr("width",  width + margin.right + margin.left)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-			.attr("class", "bottom axis")
-			.attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-			.call(bottomAxis);;
-
-
+		.attr("height", height + margin.top + margin.bottom);
 
 d3.json("data/buildings.json").then((data) => {
 	data.forEach((d)=>{
 		d.height = +d.height;
 	});
+
 	var x = d3.scaleBand()
 		.domain(data.map((d) => { return d.name }) )
 		.range([0,400])
 		.paddingInner(0.3)
 		.paddingOuter(0.3);
 	var y = d3.scaleLinear()
-		.domain([0 ,828])
-		.range([0,400]);
+						.domain([828 ,0])
+						.range([0,height]);
 	var color = d3.scaleOrdinal()
 		.domain(data.map((d)=>{ return d.name }))
 		.range(d3.schemeSet3);
+
+	var yAxisCall = d3.axisLeft(y).ticks(5)
+
+		.tickFormat((d) => { return d + "m"; });
+
+	svg.append("g")
+				.attr("class", "bottom axis")
+				.attr("transform", "translate(350, " + height+ ")")
+				.call(d3.axisBottom(x))
+		.selectAll("text")
+				.attr("y", "10")
+				.attr("x", "-5")
+				.attr("text-anchor", "end")
+				.attr("transform", "rotate(-40)");
+	svg.append("g")
+				.attr("class", "left axis")
+				.attr("transform", "translate(350,0)")
+				.call(yAxisCall);
+	svg.append("text")
+				.attr("text-anchor", "end")
+				.attr("x", width/2+200)
+				.attr("y", height+130)
+				.text("The word's tallest buildings");
+	svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", margin.left+200)
+    .attr("x", margin.top-350)
+    .text("Height (m)");
+
+
 	var rects = svg.selectAll("rect")
+
 		.data(data)
 		.enter()
+
 		.append("rect")
-		.attr("y", 0)
-		.attr("x", (d)=>{
-		return x(d.name);
-		})
+		.attr("transform", "translate(350,0)")
+		.attr("y", (d) => { return y(d.height); })
+		.attr("x", (d) => { return x(d.name); })
 		.attr("width", x.bandwidth)
-		.attr("height", (d)=>{
-		return y(d.height);
-		})
-		.attr("fill", (d) => { return color(d.name) } );
+		.attr("height", (d) => { return height - y(d.height); })
+		.attr("fill", "grey");
+
 });
