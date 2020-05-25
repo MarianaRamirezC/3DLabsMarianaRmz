29f7c75bebe855776a84c53f113a1c3051886453
mariana <0196539@up.edu.mx>
2020-05-25 15:36:10 -0500

    proyect01 listo

--- d3Labs/projects/brewery/js/main.js
+++ d3Labs/projects/brewery/js/main.js
@@ -1,7 +1,6 @@
-var margin = {top: 30, right: 10, bottom: 170, left:50};
-var width = 400;
+var margin = {top: 30, right: 0, bottom: 170, left:100};
+var width = 450;
 var height = 500;
-
 var svg = d3.select("#chart-area").append("svg")
 		.attr("width",  width + margin.right + margin.left)
 		.attr("height", height + margin.top + margin.bottom);
@@ -17,7 +16,7 @@ d3.json("data/revenues.json").then((data) => {
 		.paddingInner(0.3)
 		.paddingOuter(0.3);
 	var y = d3.scaleLinear()
-						.domain([50 ,0])
+						.domain([d3.max(data,(d)=>{return d.revenue;}) ,0])
 						.range([0,height]);
 	var color = d3.scaleOrdinal()
 		.domain(data.map((d)=>{ return d.month}))
@@ -25,11 +24,11 @@ d3.json("data/revenues.json").then((data) => {
 
 	var yAxisCall = d3.axisLeft(y).ticks(12)
 
-		.tickFormat((d) => { return d + "k"; });
+		.tickFormat((d) => { return "$" +d/1000+ "k"; });
 
 	svg.append("g")
 				.attr("class", "bottom axis")
-				.attr("transform", "translate(50, " + height+ ")")
+				.attr("transform", "translate(100, " + height+ ")")
 				.call(d3.axisBottom(x))
 		.selectAll("text")
 				.attr("y", "10")
@@ -38,7 +37,7 @@ d3.json("data/revenues.json").then((data) => {
 				.attr("transform", "rotate(-40)");
 	svg.append("g")
 				.attr("class", "left axis")
-				.attr("transform", "translate(50,0)")
+				.attr("transform", "translate(100,0)")
 				.call(yAxisCall);
 	svg.append("text")
 				.attr("text-anchor", "end")
@@ -48,22 +47,19 @@ d3.json("data/revenues.json").then((data) => {
 	svg.append("text")
         .attr("text-anchor", "end")
         .attr("transform", "rotate(-90)")
-        .attr("y", margin.left-40)
+        .attr("y", margin.left-70)
         .attr("x", margin.top-200)
         .text("Revenues (dlls)");
-
-
 	var rects = svg.selectAll("rect")
-
 		.data(data)
 		.enter()
-
 		.append("rect")
-		.attr("transform", "translate(50,0)")
+		.attr("transform", "translate(100,0)")
 		.attr("y", (d) => { return y(d.revenue); })
 		.attr("x", (d) => { return x(d.month); })
 		.attr("width", x.bandwidth)
 		.attr("height", (d) => { return height - y(d.revenue); })
 		.attr("fill", "yellow");
+
 
 });
